4e9e940e565edc4cd3c7c05ea0f59e8354630071
newnamtab <newnamtab@users.noreply.github.com>
2026-04-22 14:19:09 +0200

    Added initial basic user account frontend

---
+++ WebApp/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Login
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
